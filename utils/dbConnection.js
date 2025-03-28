3538192143aadecbf7f9a3ae3db907dd5590807c
prochaosdevv <info@himotechglobal.com>
2025-03-28 22:43:53 +0530

    updated

--- utils/dbConnection.js
+++ utils/dbConnection.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config();
 
-const uri = "mongodb+srv://" + process.env.DB_USER + ":" + process.env.DB_PASSWORD + "@" + process.env.DB_URI + "/"+process.env.DB_NAME+"?retryWrites=true&w=majority";
-// const uri = "mongodb://127.0.0.1:27017/"
+// const uri = "mongodb+srv://" + process.env.DB_USER + ":" + process.env.DB_PASSWORD + "@" + process.env.DB_URI + "/"+process.env.DB_NAME+"?retryWrites=true&w=majority";
+const uri = "mongodb://127.0.0.1:27017/"
 console.log(uri);
 // const uri = 'mongodb://fyoutub7:'+ process.env.DB_PASSWORD + '@ac-zso3jtk-shard-00-00.kl8qt2p.mongodb.net:27017,ac-zso3jtk-shard-00-01.kl8qt2p.mongodb.net:27017,ac-zso3jtk-shard-00-02.kl8qt2p.mongodb.net:27017/'+process.env.DB_NAME+'?ssl=true&replicaSet=atlas-iwlbia-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0'
 
